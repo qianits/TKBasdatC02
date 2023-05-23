6a879862063e35550638b322a4d9a446d25f5274
Qistina Muharrifa <qistinamuharrifa@qistina.local>
2023-05-23 07:48:25 +0700

    qistina ngetes

--- penonton/views.py
+++ penonton/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def dashboard_penonton(request):
     return render(request, 'dashboard_penonton.html')
 
+def pembelian_tiket(request):
+    return render(request, 'pembelian_tiket.html')
+
+
