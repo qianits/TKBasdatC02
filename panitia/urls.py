7b058561189d4aeed59488037ce12dbb8c20bd02
Muhammad Faras Baginda <muhammad.faras@ui.ac.id>
2023-05-25 11:36:09 +0700

    Progress CR Mulai Rapat

--- panitia/urls.py
+++ panitia/urls.py
@@ -5,4 +5,6 @@ app_name = 'panitia'
 
 urlpatterns = [
     path('', dashboard_panitia, name='dashboard'),
+    path('rapat_pertandingan/', daftar_pertandingan, name='daftar_pertandingan'),
+    path('mulai-rapat/<uuid:id_pertandingan>/', mulai_rapat, name='mulai_rapat'),
 ]
